00bc4fedbd02d327e12d1b305f75b4d61b5616c0
Antonio <tzamora@gmail.com>
2015-02-02 20:21:28 -0600

    added restart button

---
+++ Assets/GameContext.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameContext : MonoBehaviour {
+
+	void OnGUI(){
+
+		if (GUILayout.Button ("Restart")) {
+			
+			Application.LoadLevel(0);
+			
+		}
+
+	}
+}

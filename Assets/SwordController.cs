b3df303d3b00fd51c2c75c5354857a5d56761399
Antonio <tzamora@gmail.com>
2015-02-11 20:20:58 -0600

    asd

---
+++ Assets/SwordController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		SwingRoutine ();
+
+	}
+	
+	// Update is called once per frame
+	void SwingRoutine (){
+	
+		this.ttLoop (0.5f, delegate(ttHandler handler){
+
+			transform.localPosition = Vector3.Slerp(transform.localPosition, transform.localPosition + new Vector3(1, 0, 1), handler.t);
+
+		});
+
+		//torta
+	}
+}

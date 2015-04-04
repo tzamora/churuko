6a9c9bcea001d1b469cf131433bc77362caa147f
Antonio <tzamora@gmail.com>
2015-04-03 18:31:31 -0600

    update churuko

---
+++ Assets/scripts/FlowerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerController : MonoBehaviour {
+
+	public GameObject petalsPivot;
+
+	public float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+		MoveRoutine ();
+
+	}
+
+	void MoveRoutine(){
+
+		this.ttLoop (delegate(ttHandler handlero) {
+
+			petalsPivot.transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * speed ));
+
+		});
+
+	}
+}
---
+++ Assets/scripts/TriggerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TriggerController : MonoBehaviour {
+
+	public bool isColliding = false;
+
+	public bool isEmpty = false;
+
+	public Action<Collider> OnEnter;
+
+	public Action<Collider> OnExit;
+
+	public List<Collider> others = null;
+
+	void OnTriggerEnter(Collider theOther) {
+
+		isColliding = true;
+
+		isEmpty = false;
+
+		others.Add(theOther);
+
+		if (OnEnter!=null) {
+			OnEnter(theOther);
+		}
+
+	}
+
+	void OnTriggerExit(Collider theOther) {
+
+		others.Remove (theOther);
+
+		if (others.Count <= 0) {
+			
+			isColliding = false;
+			isEmpty = true;
+			
+		}
+
+		if (OnExit!=null) {
+			OnExit(theOther);		
+		}
+	}
+
+}

29117d18a0e1cdcd74e61dd18cd479d9b500a893
tzamora <tzamora@gmail.com>
2017-01-21 21:32:16 -0600

    add ui when die

---
+++ Assets/scripts/DieMenuPanelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DieMenuPanelController : MonoBehaviour {
+
+	public Button restartButton;
+
+	public Button menuButton;
+
+	// Use this for initialization
+	void Start () {
+
+		restartButton.onClick.AddListener (()=>{
+
+			Scene scene = SceneManager.GetActiveScene();
+
+			SceneManager.LoadScene(scene.name);
+
+		});
+
+		menuButton.onClick.AddListener (()=>{
+
+
+
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

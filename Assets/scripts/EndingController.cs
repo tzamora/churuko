a008a76ea6fd358dc6a89b30ce62b4e9b8c23ac0
tzamora <tzamora@gmail.com>
2017-01-22 15:28:04 -0600

    all good

---
+++ Assets/scripts/EndingController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+
+public class EndingController : MonoBehaviour {
+
+	public ColliderController endingTrigger;
+
+	// Use this for initialization
+	void Start () {
+
+		endingTrigger.TriggerEnter += (Collider collider) => {
+
+			this.tt("MoveCamera").Loop(6f, t=>{
+
+				//Camera.main.transform.movet
+
+			});
+
+		};
+
+	}
+}

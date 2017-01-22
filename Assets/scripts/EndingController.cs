3c5347408fe803b4245f6e55f4b4b745aa8bbebb
tzamora <tzamora@gmail.com>
2017-01-22 16:48:45 -0600

    added enging

--- Assets/scripts/EndingController.cs
+++ Assets/scripts/EndingController.cs
@@ -1,23 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using matnesis.TeaTime;
+using Exploder.Utils;
+using UnityEngine.SceneManagement;
 
 
 public class EndingController : MonoBehaviour {
 
 	public ColliderController endingTrigger;
 
+	public GameObject reactor;
+
+	public AudioClip reactorSound;
+
+	public Text finalText;
+
 	// Use this for initialization
 	void Start () {
 
 		endingTrigger.TriggerEnter += (Collider collider) => {
 
-			this.tt("MoveCamera").Loop(6f, t=>{
+			Vector3 playerPosition = GameContext.Get.player.transform.position;
 
-				//Camera.main.transform.movet
+			SoundManager.Get.PlayClip(reactorSound, false);
+
+			Camera.main.transform.parent = null;
+
+			this.tt().Add(1f, ()=>{
+
+				//ExploderSingleton.ExploderInstance.ExplodeObject (GameContext.Get.player);
+			});
+
+			this.tt("MoveCamera").Loop(10f, t=>{
+
+				Camera.main.transform.position = 
+					Vector3.Lerp(playerPosition, reactor.transform.position, t.t);
+
+			}).Add(3f, ()=>{
+				SceneManager.LoadScene("Intro");
+			});
+
+			this.tt().Add(7f, t=>{
+				
+				Camera.main.GetComponent<FadeInOut>().fadeDirection = 1;
+			
+			});
+
+			//Color currentColor = finalText.color;
+				
+			this.tt().Add(2f).Loop (4f, t => {
+				//finalText.color = Color.
+				finalText.color = new Color(finalText.color.r, finalText.color.g, finalText.color.b, Mathf.Lerp(0f, 1f, t.t));
+
+				//print("---> " + finalText.color);
 
 			});
+
 
 		};
 
---
+++ Assets/scripts/FadeInOut.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script goes on the camera object
+public class FadeInOut : MonoBehaviour
+{
+
+    //The fade direction, -1 means from texture to no texture, 1 means the opposite
+    public float fadeDirection = -1;
+    //The speed which the fading process will be performed
+    public float fadeSpeed = 0.3f;
+    //The initial alpha for the texture
+    public float alpha = 1.0f;
+    //The texture we are going to use
+    public Texture2D fadeOutTexture;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //TODO change this so we can control it from the editor ONLY
+        //alpha=1.0f;
+        //fadeIn(); //Perhaps this should not be here
+        // fadeOutTexture = Resources.Load("UI/white") as Texture2D;
+
+        //Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Magic
+    void OnGUI()
+    {
+        alpha += fadeDirection * fadeSpeed * Time.deltaTime;// Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        UnityEngine.Color temp = GUI.color;
+        temp.a = alpha;
+        GUI.color = temp;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+
+    }
+
+    //This 2 methods change the direction of the fade
+    public void fadeIn()
+    {
+        fadeDirection = -1;
+    }
+
+
+    public void fadeOut()
+    {
+        fadeDirection = 1;
+    }
+
+    public bool AreWeDone()
+    {
+        if (fadeDirection == -1 && alpha == 0)
+        {
+            return true;
+        }
+        else if (fadeDirection == 1 && alpha == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}

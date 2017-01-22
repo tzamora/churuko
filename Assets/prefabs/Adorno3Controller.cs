1336597596094be66217b02e77166b3ace050fa2
Tanameck <Tanameck@gmail.com>
2017-01-22 02:06:26 -0600

    Adornos 2 y 3

---
+++ Assets/prefabs/Adorno3Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+public class Adorno3Controller : MonoBehaviour {
+
+	public Transform FinalPosition;
+	public Transform InitialPosition;
+	public float MovingTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+		MoveLightsRoutine ();
+
+	}
+
+	void MoveLightsRoutine(){
+
+		Vector3 startPos = transform.position;
+
+		this.tt ().Loop (MovingTime, handler => {
+			transform.position = Vector3.Lerp (startPos, FinalPosition.position, handler.t);
+
+		}).Add(1f).Loop (MovingTime, handler => {
+			
+			transform.position = Vector3.Lerp (InitialPosition.position, FinalPosition.position, handler.t);
+
+		}).Add(1f).Repeat ();
+
+	}
+}
